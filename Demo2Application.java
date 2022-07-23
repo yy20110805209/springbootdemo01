5047ab41639a09be9390106c0c8f2e5d6840d32a
deng yang <56860458+yy20110805209@users.noreply.github.com>
2022-07-23 18:14:55 +0800

    Create Demo2Application.java

---
+++ Demo2Application.java
@@ -0,0 +1,13 @@
+package com.example1.demo2;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example1.demo2.mapper")
+public class Demo2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+    }
+}
